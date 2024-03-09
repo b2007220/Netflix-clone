501214ee87cf138f4f06eb4837965eb2f3179a10
b2007220 <127931507+b2007220@users.noreply.github.com>
2024-03-09 21:55:26 +0700

    feat:auth & home ui

--- client/app/(auth)/sign-up/page.tsx
+++ client/app/(auth)/sign-up/page.tsx
@@ -3,11 +3,18 @@ import { Input } from "@/components/ui/input";
 import Link from "next/link";
 import GithubSignInButton from "../../components/GithubSignInButtion";
 import GoogleSignInButton from "../../components/GoogleSignInButton";
+import { authOptions } from "@/app/utils/auth";
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
 
-export default function SignUp() {
+export default async function SignUp() {
+  const session = await getServerSession(authOptions);
+  if (session) {
+    return redirect("/home");
+  }
   return (
     <div className="mt-24 rounded bg-black/80 py-10 px-6 md:mt-0 md:max-w-sm md:px-14">
-      <form action="">
+      <form method="post" action="/api/auth/signin">
         <h1 className="text-3xl font-semibold text-white">Sign Up</h1>
         <div className="space-y-4 mt-5">
           <Input
---
+++ client/app/home/layout.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../utils/auth";
+import { redirect } from "next/navigation";
+
+export default async function HomeLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return redirect("/login");
+  }
+  return <div>{children}</div>;
+}
